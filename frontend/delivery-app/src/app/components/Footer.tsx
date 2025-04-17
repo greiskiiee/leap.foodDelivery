0495eef6dc567982b70c6749bbf1eefc21496eb2
chimegerdene <chimgeebatuh@gmail.com>
2025-04-17 10:09:38 +0800

    footer

--- frontend/delivery-app/src/app/components/Footer.tsx
+++ frontend/delivery-app/src/app/components/Footer.tsx
@@ -1,26 +1,31 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 export const Footer = () => {
   const footerText: string = "Fresh fast delivered ";
   return (
     <div className="w-full h-fit bg-[#18181B] flex flex-col">
-      <div className="bg-[#EF4444] flex w-full h-fit py-7 gap-[34px] px-[98px] mt-[60px] ">
-        <p className="inter font-[600] text-[30px] text-[#FAFAFA]">
+      <div className="bg-[#EF4444] flex w-full h-[92px] py-7 gap-[34px] px-[98px] mt-[60px] overflow-x-hidden">
+        <p className="w-[272px] inter font-[600] text-[30px] text-[#FAFAFA]">
           {footerText}
         </p>
-        <p className="inter font-[600] text-[30px] text-[#FAFAFA]">
+        <p className="w-[272px] inter font-[600] text-[30px] text-[#FAFAFA]">
           {footerText}
         </p>
-        <p className="inter font-[600] text-[30px] text-[#FAFAFA]">
+        <p className="w-[272px] inter font-[600] text-[30px] text-[#FAFAFA]">
           {footerText}
         </p>
-        <p className="inter font-[600] text-[30px] text-[#FAFAFA]">
+        <p className="w-[272px] inter font-[600] text-[30px] text-[#FAFAFA]">
           {footerText}
         </p>
-        <p className="inter font-[600] text-[30px] text-[#FAFAFA]">
+        <p className="w-[272px] inter font-[600] text-[30px] text-[#FAFAFA]">
           {footerText}
         </p>
-        <p className="inter font-[600] text-[30px] text-[#FAFAFA]">
+        <p className="w-[272px] inter font-[600] text-[30px] text-[#FAFAFA]">
+          {footerText}
+        </p>
+        <p className="w-[272px] inter font-[600] text-[30px] text-[#FAFAFA]">
           {footerText}
         </p>
       </div>
@@ -59,27 +64,79 @@ export const Footer = () => {
             </p>
           </div>
 
-          <div className="flex flex-col w-[228px] gap-4 justify-end">
+          <div className="flex flex-col gap-4 justify-end">
+            <div>
+              <p className="h-[28px] text-[16px] font-[400] text-[#71717A]">
+                MENU
+              </p>
+            </div>
+            <div className="flex gap-[36px]">
+              <div className="w-[228px] flex flex-col gap-4 justify-end">
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Appetizers
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Salads
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Pizzas
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Lunch favourites
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Main dishes
+                </p>
+              </div>
+
+              <div className="w-[228px] flex flex-col gap-4 justify-end">
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Side dishes
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Brunch
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Desserts
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Beverages
+                </p>
+                <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
+                  Fish & sea foods
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 justify-start items-start">
             <p className="h-[28px] text-[16px] font-[400] text-[#71717A]">
-              MENU
+              FOLLOW US
             </p>
-            <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
-              Appetizers
-            </p>
-            <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
-              Salads
-            </p>
-            <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
-              Pizzas
-            </p>
-            <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
-              Lunch favourites
-            </p>
-            <p className="h-[24px] text-[16px] font-[400] text-[#FAFAFA]">
-              Main dishes
-            </p>
+            <div className="flex gap-4">
+              <FaFacebook size={28} />
+              <FaInstagram size={28} />
+            </div>
           </div>
         </div>
+      </div>
+      <div className="w-full h-fit py-8 flex flex-col gap-10   px-[98px]">
+        <div className="w-[98%] border-t border-t-[#F4F4F5]"></div>
+        <div className="flex gap-12">
+          <p className="inter text-[14px] font-[400] text-[#71717A]">
+            Copy right 2024 © Nomnom LLC
+          </p>
+          <p className="inter text-[14px] font-[400] text-[#71717A]">
+            Privacy policy{" "}
+          </p>
+          <p className="inter text-[14px] font-[400] text-[#71717A]">
+            Terms and conditoin
+          </p>
+          <p className="inter text-[14px] font-[400] text-[#71717A]">
+            Cookie policy
+          </p>
+        </div>
+        <div className="h-[100px]"></div>
       </div>
     </div>
   );
